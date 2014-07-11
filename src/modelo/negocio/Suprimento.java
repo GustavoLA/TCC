697e97c48c99a320238d40635bcbca6dcce32d41
GUSTAVO <GUSTAVO@GUSTAVO-PC>
2014-07-11 16:37:41 -0300

    Campos valor; notaFiscal; dtVencimento; unidadeMedida adicionados e funcionais

--- src/modelo/negocio/Suprimento.java
+++ src/modelo/negocio/Suprimento.java
@@ -1,9 +1,11 @@
 package modelo.negocio;
 
+import java.util.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.Temporal;
 
 @Entity
 public class Suprimento {
@@ -13,10 +15,55 @@ public class Suprimento {
     private int codigo;
     private int qtdade;
     private String nome;
+    private Double valor;
+    private String unidadeMedida;
+    private char notaFiscal;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dtVencimento;
     @Lob
     private String descricao;
     private char producao;
 
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getUnidadeMedida() {
+        return unidadeMedida;
+    }
+
+    public void setUnidadeMedida(String unidadeMedida) {
+        this.unidadeMedida = unidadeMedida;
+    }
+
+    public char getNotaFiscal() {
+        return notaFiscal;
+    }
+
+    public void setNotaFiscal(char notaFiscal) {
+        this.notaFiscal = notaFiscal;
+    }
+
+    public Date getDtVencimento() {
+        return dtVencimento;
+    }
+
+    public void setDtVencimento(Date dtVencimento) {
+        this.dtVencimento = dtVencimento;
+    }
+
+    
+    
+    
+    /**
+     * OS que estão em cima foram adc agr.
+     * @return 
+     */
+    
     public int getCodigo() {
         return codigo;
     }
--- src/view/inserir/InserirSuprimento.java
+++ src/view/inserir/InserirSuprimento.java
@@ -1,11 +1,13 @@
 package view.inserir;
 
 import controller.SuprimentoController;
+import java.text.SimpleDateFormat;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 import modelo.negocio.Suprimento;
 
 public class InserirSuprimento extends javax.swing.JFrame {
-    
+
     private DefaultTableModel modelo;
     private int linhaSelecionada;
 
@@ -38,6 +40,7 @@ public class InserirSuprimento extends javax.swing.JFrame {
     private void initComponents() {
 
         bgProducao = new javax.swing.ButtonGroup();
+        bgNotaFiscal = new javax.swing.ButtonGroup();
         painelFundo = new javax.swing.JPanel();
         cadastroSuprimento = new javax.swing.JTabbedPane();
         produto = new javax.swing.JPanel();
@@ -53,6 +56,15 @@ public class InserirSuprimento extends javax.swing.JFrame {
         producao = new javax.swing.JLabel();
         rbSim = new javax.swing.JRadioButton();
         rbNao = new javax.swing.JRadioButton();
+        valor = new javax.swing.JLabel();
+        txValor = new javax.swing.JTextField();
+        unidadeMedida = new javax.swing.JLabel();
+        txUnidadeMedida = new javax.swing.JTextField();
+        notaFiscal = new javax.swing.JLabel();
+        rbSim1 = new javax.swing.JRadioButton();
+        rbNao1 = new javax.swing.JRadioButton();
+        dataValidade = new javax.swing.JLabel();
+        txDtValidade = new javax.swing.JFormattedTextField();
         btSalvar = new javax.swing.JButton();
         btLimpar = new javax.swing.JButton();
 
@@ -90,38 +102,84 @@ public class InserirSuprimento extends javax.swing.JFrame {
         bgProducao.add(rbNao);
         rbNao.setText("NÃO");
 
+        valor.setText("VALOR");
+
+        unidadeMedida.setText("UNIDADE MEDIDA");
+
+        notaFiscal.setText("NOTA FISCAL");
+
+        rbSim1.setBackground(new java.awt.Color(255, 255, 255));
+        bgNotaFiscal.add(rbSim1);
+        rbSim1.setText("SIM");
+
+        rbNao1.setBackground(new java.awt.Color(255, 255, 255));
+        bgNotaFiscal.add(rbNao1);
+        rbNao1.setText("NÃO");
+
+        dataValidade.setText("DATA VENCIMENTO");
+
+        try {
+            txDtValidade.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("##/##/####")));
+        } catch (java.text.ParseException ex) {
+            ex.printStackTrace();
+        }
+
         javax.swing.GroupLayout produtoLayout = new javax.swing.GroupLayout(produto);
         produto.setLayout(produtoLayout);
         produtoLayout.setHorizontalGroup(
             produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(produtoLayout.createSequentialGroup()
+                .addContainerGap()
                 .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(produtoLayout.createSequentialGroup()
-                        .addContainerGap()
+                        .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(descricao)
+                            .addComponent(nome))
+                        .addGap(18, 18, 18)
+                        .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(scrollDescricao, javax.swing.GroupLayout.DEFAULT_SIZE, 458, Short.MAX_VALUE)
+                            .addComponent(txNome)))
+                    .addGroup(produtoLayout.createSequentialGroup()
+                        .addComponent(id)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(txId, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(produtoLayout.createSequentialGroup()
                         .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(produtoLayout.createSequentialGroup()
                                 .addComponent(qntdade)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(txQntdade, javax.swing.GroupLayout.PREFERRED_SIZE, 135, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(produtoLayout.createSequentialGroup()
+                                .addGap(10, 10, 10)
                                 .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(descricao)
-                                    .addComponent(nome))
-                                .addGap(18, 18, 18)
-                                .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(scrollDescricao, javax.swing.GroupLayout.DEFAULT_SIZE, 458, Short.MAX_VALUE)
-                                    .addComponent(txNome)))
+                                    .addGroup(produtoLayout.createSequentialGroup()
+                                        .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addGroup(produtoLayout.createSequentialGroup()
+                                                .addComponent(valor)
+                                                .addGap(18, 18, 18)
+                                                .addComponent(txValor))
+                                            .addGroup(produtoLayout.createSequentialGroup()
+                                                .addComponent(producao)
+                                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                                .addComponent(rbSim)))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(rbNao))
+                                    .addGroup(produtoLayout.createSequentialGroup()
+                                        .addComponent(dataValidade)
+                                        .addGap(18, 18, 18)
+                                        .addComponent(txDtValidade, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                        .addGap(18, 18, 18)
+                        .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(produtoLayout.createSequentialGroup()
-                                .addComponent(id)
+                                .addComponent(notaFiscal)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(txId, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                    .addGroup(produtoLayout.createSequentialGroup()
-                        .addGap(20, 20, 20)
-                        .addComponent(producao)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(rbSim)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(rbNao)))
+                                .addComponent(rbSim1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(rbNao1))
+                            .addGroup(produtoLayout.createSequentialGroup()
+                                .addComponent(unidadeMedida)
+                                .addGap(18, 18, 18)
+                                .addComponent(txUnidadeMedida, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE)))))
                 .addContainerGap(111, Short.MAX_VALUE))
         );
         produtoLayout.setVerticalGroup(
@@ -141,13 +199,26 @@ public class InserirSuprimento extends javax.swing.JFrame {
                 .addGap(28, 28, 28)
                 .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(qntdade)
-                    .addComponent(txQntdade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(txQntdade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(unidadeMedida)
+                    .addComponent(txUnidadeMedida, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(43, 43, 43)
                 .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(producao)
                     .addComponent(rbSim)
-                    .addComponent(rbNao))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 172, Short.MAX_VALUE)
+                    .addComponent(rbNao)
+                    .addComponent(notaFiscal)
+                    .addComponent(rbSim1)
+                    .addComponent(rbNao1))
+                .addGap(18, 18, 18)
+                .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(valor)
+                    .addComponent(txValor, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(dataValidade)
+                    .addComponent(txDtValidade, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 96, Short.MAX_VALUE)
                 .addGroup(produtoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(id)
                     .addComponent(txId, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -222,25 +293,44 @@ public class InserirSuprimento extends javax.swing.JFrame {
     }//GEN-LAST:event_txIdActionPerformed
 
     private void btSalvarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btSalvarActionPerformed
-        
+
         Suprimento s = new Suprimento();
-        
+
+        s.setValor(Double.parseDouble(txValor.getText()));
+        s.setUnidadeMedida(txUnidadeMedida.getText());
+
+        try {
+            String data = txDtValidade.getText();
+            s.setDtVencimento(new SimpleDateFormat("dd/mm/yyyy").parse(data));
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "FORMATO INVÁLIDO! UTILIZE: DD/MM/YYYY\n" + ex);
+        }
+
+        if (rbNao1.isSelected()) {
+            s.setNotaFiscal('N');
+        } else {
+            s.setNotaFiscal('S');
+        }
+
+        /**
+         * Pra cima feito agr.
+         */
         s.setDescricao(txDescricao.getText());
         s.setNome(txNome.getText());
         s.setQtdade(Integer.parseInt(txQntdade.getText()));
-        
+
         if (rbNao.isSelected()) {
             s.setProducao('N');
         } else {
             s.setProducao('S');
         }
-        
+
         SuprimentoController sc = new SuprimentoController();
-        
+
         if (!(txId.getText().equals("") | (txId.getText().equals(null)))) {
             s.setCodigo(Integer.parseInt(txId.getText()));
             sc.salvar(s);
-            
+
             modelo.removeRow(linhaSelecionada);
             modelo.addRow(new Object[]{s.getCodigo(), s.getDescricao(), s.getNome(), s.getQtdade()});
             dispose();
@@ -251,7 +341,7 @@ public class InserirSuprimento extends javax.swing.JFrame {
     }//GEN-LAST:event_btSalvarActionPerformed
 
     private void btLimparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btLimparActionPerformed
-        
+
         txDescricao.setText(null);
         txNome.setText(null);
         txQntdade.setText(null);
@@ -260,24 +350,34 @@ public class InserirSuprimento extends javax.swing.JFrame {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.ButtonGroup bgNotaFiscal;
     private javax.swing.ButtonGroup bgProducao;
     private javax.swing.JButton btLimpar;
     private javax.swing.JButton btSalvar;
     private javax.swing.JTabbedPane cadastroSuprimento;
+    private javax.swing.JLabel dataValidade;
     private javax.swing.JLabel descricao;
     private javax.swing.JLabel id;
     private javax.swing.JLabel nome;
+    private javax.swing.JLabel notaFiscal;
     private javax.swing.JPanel painelFundo;
     private javax.swing.JLabel producao;
     private javax.swing.JPanel produto;
     private javax.swing.JLabel qntdade;
     private javax.swing.JRadioButton rbNao;
+    private javax.swing.JRadioButton rbNao1;
     private javax.swing.JRadioButton rbSim;
+    private javax.swing.JRadioButton rbSim1;
     private javax.swing.JScrollPane scrollDescricao;
     private javax.swing.JTextPane txDescricao;
+    private javax.swing.JFormattedTextField txDtValidade;
     private javax.swing.JTextField txId;
     private javax.swing.JTextField txNome;
     private javax.swing.JTextField txQntdade;
+    private javax.swing.JTextField txUnidadeMedida;
+    private javax.swing.JTextField txValor;
+    private javax.swing.JLabel unidadeMedida;
+    private javax.swing.JLabel valor;
     // End of variables declaration//GEN-END:variables
 
     private void preencherCampos(int linhaSelecionada1, int idSuprimento) {
@@ -290,16 +390,16 @@ public class InserirSuprimento extends javax.swing.JFrame {
         txDescricao.setText(s.getDescricao());
         txNome.setText(s.getNome());
         txQntdade.setText(String.valueOf(s.getQtdade()));
-        
+
         if (s.getProducao() == 'N') {
             rbNao.setSelected(true);
         } else {
             rbSim.setSelected(true);
         }
     }
-    
+
     private void bloquearCampos() {
-        
+
         txDescricao.setEditable(false);
         txNome.setEditable(false);
         txQntdade.setEditable(false);
@@ -307,6 +407,6 @@ public class InserirSuprimento extends javax.swing.JFrame {
         rbSim.setEnabled(false);
         btLimpar.setEnabled(false);
         btSalvar.setEnabled(false);
-        
+
     }
 }
